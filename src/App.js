7a3b29c5d4ba6d2983bdccfff0fbe649b56d5c17
sistatress <63374167+sistatress@users.noreply.github.com>
2020-10-25 19:57:24 +0100

    Decrement Counters - Function subtract add to App to decrement counters values. - App run : ok

--- src/App.js
+++ src/App.js
@@ -3,30 +3,55 @@ import "./styles.css";
 import Counter from "./Components/Counter";
 
 const App = () => {
-  const [counters, setCounts] = useState([{ id: 0, value: 0 },{ id: 1, value: 0 }, { id: 2, value: 0 }]); // Use by App
+  const [counters, setCounts] = useState([
+    { id: 0, value: 0 },
+    { id: 1, value: 0 },
+    { id: 2, value: 0 }
+  ]); // Use by App
 
   // Increment counter
-  const add = (id,value) => {
-    
+  const add = (id, value) => {
     const result = value + 1;
-    // Find the counter 
-    const counterIndex = counters.findIndex(counterElement => counterElement.id === id)
+    // Find the counter
+    const counterIndex = counters.findIndex(
+      (counterElement) => counterElement.id === id
+    );
     // Copy of the counters array
-    const newCounters = [...counters]
+    const newCounters = [...counters];
     // Udate the counter value
-    newCounters[counterIndex] = {...newCounters[counterIndex], value:result}
+    newCounters[counterIndex] = { ...newCounters[counterIndex], value: result };
     // Set counters state
-    setCounts(newCounters)
+    setCounts(newCounters);
+  };
+
+  // Decrement counter
+  const subtract = (id, value) => {
+    const result = value - 1;
+    // Find the counter
+    const counterIndex = counters.findIndex(
+      (counterElement) => counterElement.id === id
+    );
+    // Copy of the counters array
+    const newCounters = [...counters];
+    // Udate the counter value
+    newCounters[counterIndex] = { ...newCounters[counterIndex], value: result };
+    // Set counters state
+    setCounts(newCounters);
   };
 
   const showCounts = (element, index) => {
     //console.log(`element id: ${element.id} value: ${element.value} index: ${index}`)
     return (
       <div key={index}>
-        <Counter id={element.id} value={element.value} onAdd={add} />
+        <Counter
+          id={element.id}
+          value={element.value}
+          onAdd={add}
+          onSubtract={subtract}
+        />
       </div>
-    )
-  }
+    );
+  };
   console.log(`[Render App]`);
 
   return (
--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,16 +1,17 @@
 import React, { useEffect } from "react";
 
-const Counter = ({ id, value, onAdd }) => {
+const Counter = ({ id, value, onAdd, onSubtract }) => {
   useEffect(() => {
     console.log(`Counter Effect id ${id}`);
-  }, [value,id]);
+  }, [value, id]);
   console.log(`Counter render`);
   return (
     <div>
       <h2>
         Counter {id} : {value}
       </h2>
-      <button onClick={() => onAdd(id,value)}>+</button>
+      <button onClick={() => onSubtract(id, value)}>-</button>
+      <button onClick={() => onAdd(id, value)}>+</button>
     </div>
   );
 };
